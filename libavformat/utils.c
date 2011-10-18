3824f3bda0b93e85c01b83021cc6bed14186b4b5
Elan Feingold <elan@plexapp.com>
2011-10-18 12:33:17 -1000

    Patch 0020

--- libavformat/utils.c
+++ libavformat/utils.c
@@ -2157,7 +2157,7 @@ static int has_codec_parameters(AVCodecContext *avctx)
         val = 1;
         break;
     }
-    return avctx->codec_id != CODEC_ID_NONE && val != 0;
+    return avctx->codec_id != CODEC_ID_PROBE && val != 0;
 }
 
 static int has_decode_delay_been_guessed(AVStream *st)
